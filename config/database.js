dbf4db9cc669921413fc9913cd77ae196df5ca8b
prudhvishankar <vsprudhvi@gmail.com>
2020-06-24 19:27:37 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': '127.0.0.1', // localde iseniz bu , local değilse ip adresini yazınız .
+        'host': "{{ dbhost }}", // localde iseniz bu , local değilse ip adresini yazınız .
         'user': 'root', // kullanıcı adı 
-        'password': 'password', // şifreniz 
-        'database': 'dbname'// database ismi .
+        'password': '', // şifreniz 
+        'database': 'login'// database ismi .
     },
-	'database': 'dbname',
+	'database': 'login',
     
 };
 
