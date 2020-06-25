ab2aeeab07a0c8eab07a7bf3bca3da96f1134ab1
prudhvishankar <vsprudhvi@gmail.com>
2020-06-25 17:02:42 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': "107.23.166.210", // localde iseniz bu , local değilse ip adresini yazınız .
+        'host': "{{ dbhost }}", // localde iseniz bu , local değilse ip adresini yazınız .
         'user': 'root', // kullanıcı adı 
         'password': '', // şifreniz 
         'database': 'login'// database ismi .
